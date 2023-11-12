1f11d85280db407368aee4036efe3c7feb8e117e
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2023-11-12 16:21:17 +0100

    Initial Commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,42 @@
-// STARTER CODE FROM learnopengl.com
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <glm.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define numVAOs 1
+#define vertShaderPath "shaders/vertShader.glsl"
+#define fragShaderPath "shaders/fragShader.glsl"
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+GLuint createShaders();
+void init(GLFWwindow *window);
+void display(GLFWwindow *window, double currentTime);
+std::string readShaderSource(const char *filePath);
+void printShaderLog(GLuint shader);
+bool checkOpenGLError();
+void printProgramLog(int prog);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int main() {
-	// glfw: initialize and configure
-	// ------------------------------
+int main()
+{
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-	// glfw window creation
-	// --------------------
-	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-	if (window == NULL) {
+	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Test", NULL, NULL);
+	if (window == NULL)
+	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -35,24 +44,26 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
 		return -1;
 	}
 
+	init(window);
+
 	// render loop
 	// -----------
-	while (!glfwWindowShouldClose(window)) {
+	while (!glfwWindowShouldClose(window))
+	{
 		// input
 		// -----
 		processInput(window);
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		display(window, glfwGetTime());
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
@@ -62,21 +73,144 @@ int main() {
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
 	// ------------------------------------------------------------------
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) {
+void processInput(GLFWwindow *window)
+{
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
 	// make sure the viewport matches the new window dimensions; note that width and
 	// height will be significantly larger than specified on retina displays.
 	glViewport(0, 0, width, height);
+}
+
+GLuint createShaders()
+{
+	GLint vertCompiled;
+	GLint fragCompiled;
+	GLint linked;
+
+	std::string vertShaderStr = readShaderSource(vertShaderPath);
+	std::string fragShaderStr = readShaderSource(fragShaderPath);
+
+	const char *vertShaderSrc = vertShaderStr.c_str();
+	const char *fragShaderSrc = fragShaderStr.c_str();
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+	glCompileShader(vShader);
+	checkOpenGLError();
+	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
+	if (vertCompiled != 1)
+	{
+		std::cout << "vertex compilation failed" << std::endl;
+		printShaderLog(vShader);
+	}
+	glCompileShader(fShader);
+	checkOpenGLError();
+	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
+	if (fragCompiled != 1)
+	{
+		std::cout << "fragment compilation failed" << std::endl;
+		printShaderLog(fShader);
+	}
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
+	if (linked != 1)
+	{
+		std::cout << "linking failed" << std::endl;
+		printProgramLog(vfProgram);
+	}
+
+	return vfProgram;
+}
+void init(GLFWwindow *window)
+{
+	renderingProgram = createShaders();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+
+void display(GLFWwindow *window, double currentTime)
+{
+	glUseProgram(renderingProgram);
+	glPointSize(30.0f);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+std::string readShaderSource(const char *filePath)
+{
+	std::cout << "LOADING SHADER: " << filePath << std::endl;
+	std::string content;
+	std::ifstream fileStream(filePath, std::ios::in);
+	std::string line;
+	while (!fileStream.eof())
+	{
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+	fileStream.close();
+	return content;
+}
+
+// displays the contents of OpenGL's log when GLSL compilation failed
+void printShaderLog(GLuint shader)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0)
+	{
+		log = (char *)malloc(len);
+		glGetShaderInfoLog(shader, len, &chWrittn, log);
+		std::cout << "Shader Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+// displays the contents of OpenGL's log when GLSL linking failed
+void printProgramLog(int prog)
+{
+	int len = 0;
+	int chWrittn = 0;
+	char *log;
+	glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &len);
+	if (len > 0)
+	{
+		log = (char *)malloc(len);
+		glGetProgramInfoLog(prog, len, &chWrittn, log);
+		std::cout << "Program Info Log: " << log << std::endl;
+		free(log);
+	}
+}
+
+// checks the OpenGL error flag for the occurrrence of an OpenGL error
+// detects both GLSL compilation errors and OpenGL runtime errors
+bool checkOpenGLError()
+{
+	bool foundError = false;
+	int glErr = glGetError();
+	while (glErr != GL_NO_ERROR)
+	{
+		std::cout << "glError: " << glErr << std::endl;
+		foundError = true;
+		glErr = glGetError();
+	}
+	return foundError;
 }
